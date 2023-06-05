57f754e8a8739156f968e52ed99dc3a255952908
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-06-05 11:23:25 -0600

    Update sss1.sql

--- sss1.sql
+++ sss1.sql
@@ -1,24 +1,171 @@
--- sql dump
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2023 at 07:22 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- DB --> sss1 (server side scripting 1)
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sss1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
 
 CREATE TABLE `contactus` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `message` text NOT NULL,
-  `ifread` bit NOT NULL
+  `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `contactus`
+--
+
 INSERT INTO `contactus` (`id`, `name`, `email`, `message`) VALUES
-(1, 'test1', 'test1@mail.com', 'Test 1 Message');
+(1, 'test1', 'test1@mail.com', 'Test 1 Message'),
+(9, 'name', 'email', 'message'),
+(10, '1', '2', '3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`first_name`, `last_name`, `position`, `id`) VALUES
+('nikithA', 'mandarappalU', 'CFO', 2),
+('Richard', 'Nixon', 'Advisor', 3),
+('yah', 'aaaaaaaaa', 'Advisor', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `value` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `type`, `value`) VALUES
+(1, 'Government housing bond', 500000),
+(2, 'Government housing bond 2', 1600),
+(3, 'Small business stock', 1),
+(4, 'Bitcoin stock', 8789789),
+(5, 'Blackjack bet', 20),
+(6, 'Crypto stock', 200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
 
 CREATE TABLE `reviews` (
   `id` int(11) NOT NULL,
   `testimonial` text NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `stars` int(11) NOT NULL
+  `author` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `testimonial`, `author`) VALUES
+(1, 'disgoustang service. Never gonna come back here again ew', 'brodda'),
+(2, 'got super rich and spent it all on charity', 'wethebestmusic');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
